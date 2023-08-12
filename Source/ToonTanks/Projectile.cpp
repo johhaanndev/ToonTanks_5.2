8844eec22fc09c0f2eac28cac408e092f62f2d88
johhaanndev <71643284+johhaanndev@users.noreply.github.com>
2023-08-12 18:39:29 +0200

    Projectile Movement Component

--- Source/ToonTanks/Projectile.cpp
+++ Source/ToonTanks/Projectile.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Projectile.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 
 // Sets default values
 AProjectile::AProjectile()
@@ -11,6 +12,10 @@ AProjectile::AProjectile()
 
 	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Projectile Mesh"));
 	RootComponent = ProjectileMesh;
+
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement Component"));
+	ProjectileMovementComponent->MaxSpeed = 1300.f;
+	ProjectileMovementComponent->InitialSpeed = 1300.f;
 }
 
 // Called when the game starts or when spawned
