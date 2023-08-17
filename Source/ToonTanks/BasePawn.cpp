bda859c82ecf928f756b02c1a9b06a368f98718b
johhaanndev <71643284+johhaanndev@users.noreply.github.com>
2023-08-17 17:54:01 +0200

    Death Particles

--- Source/ToonTanks/BasePawn.cpp
+++ Source/ToonTanks/BasePawn.cpp
@@ -29,7 +29,10 @@ ABasePawn::ABasePawn()
 void ABasePawn::HandleDestruction()
 {
 	// TODO: Visual/Sound effects
-
+	if (DeathParticles)
+	{
+		UGameplayStatics::SpawnEmitterAtLocation(this, DeathParticles, GetActorLocation(), GetActorRotation());
+	}
 	
 }
 
--- Source/ToonTanks/BasePawn.h
+++ Source/ToonTanks/BasePawn.h
@@ -37,4 +37,7 @@ private:
 
 	UPROPERTY(EditDefaultsOnly, Category = "Combat")
 	TSubclassOf<class AProjectile> ProjectileClass;
+
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	class UParticleSystem* DeathParticles;
 };
